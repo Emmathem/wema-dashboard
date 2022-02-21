7e5d53339d91bc849fad1ac9972bb1456d6ca977
Falua Temitope <emmat0616@gmail.com>
2022-02-21 23:03:28 +0100

    tale done

---
+++ src/static/recentRequests.js
@@ -0,0 +1,44 @@
+import Photo1 from '../assets/images/image1.png'
+import Photo2 from '../assets/images/image2.png'
+import Photo3 from '../assets/images/image3.png'
+import Photo4 from '../assets/images/image4.png'
+
+
+export const RecentRequests = [
+    {
+        id: '1',
+        name: 'Michael Olu',
+        email: 'michael@mail.com',
+        type: 'New Agent',
+        status: 'Pending',
+        date: '23/04/18',
+        photo: Photo1, 
+    },
+    {
+        id: '2',
+        name: 'Michael Olu',
+        email: 'michael@mail.com',
+        type: 'New Agent',
+        status: 'Pending',
+        date: '23/04/18',
+        photo: Photo2, 
+    },
+    {
+        id: '3',
+        name: 'Michael Olu',
+        email: 'michael@mail.com',
+        type: 'New Agent',
+        status: 'Active',
+        date: '23/04/18',
+        photo: Photo3, 
+    },
+    {
+        id: '4',
+        name: 'Michael Olu',
+        email: 'michael@mail.com',
+        type: 'New Agent',
+        status: 'Active',
+        date: '23/04/18',
+        photo: Photo4, 
+    },
+]
